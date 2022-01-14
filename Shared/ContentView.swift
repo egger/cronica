eb5fe3d80cafd00ba44d6501d8670da9d13a754c
Alexandre Madeira <contact@alexandremadeira.dev>
2022-01-14 20:59:14 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexandre Madeira on 14/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryApp.swift
+//  Shared
+//
+//  Created by Alexandre Madeira on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
