06544f9feb74710a4c383e8f385cc653ee937ed9
Alexandre Madeira <contact@alexandremadeira.dev>
2022-02-06 14:33:52 -0300

    Added a Full Screen Cover to Cast image.

--- Shared/View/Credits/CastView.swift
+++ Shared/View/Credits/CastView.swift
@@ -41,23 +41,51 @@ private struct DrawingConstants {
     static let imageWidth: CGFloat = 150
     static let imageHeight: CGFloat = 150
     static let biographyPadding: CGFloat = 4
+    static let imageRadius: CGFloat = 12
 }
 
 struct CastViewBody: View {
     let cast: Cast
+    @State private var showingSheet: Bool = false
     var body: some View {
         ScrollView {
             VStack {
-                AsyncImage(url: cast.image) { image in
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: DrawingConstants.imageWidth,
-                               height: DrawingConstants.imageHeight)
-                        .clipShape(Circle())
-                        .padding([.top, .bottom])
-                } placeholder: {
-                    ProgressView()
+                Button {
+                    self.showingSheet = true
+                } label: {
+                    AsyncImage(url: cast.image) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: DrawingConstants.imageWidth,
+                                   height: DrawingConstants.imageHeight)
+                            .clipShape(Circle())
+                            .padding([.top, .bottom])
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+                .fullScreenCover(isPresented: $showingSheet) {
+                    NavigationView {
+                        AsyncImage(url: cast.image) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .padding()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .toolbar {
+                            ToolbarItem(placement: .primaryAction) {
+                                Button {
+                                    self.showingSheet = false
+                                } label: {
+                                    Text("Done")
+                                        .fontWeight(.semibold)
+                                }
+                            }
+                        }
+                    }
                 }
                 GroupBox {
                     Text(cast.biography ?? "")
