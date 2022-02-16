d465482ac13b08f0385cd783a787d191dc56c90e
Alexandre Madeira <contact@alexandremadeira.dev>
2022-02-16 00:31:22 -0300

    Removed old tvOS template.

---
+++ Shared/ViewModel/TVDetailsViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  TVDetailsViewModel.swift
+//  Story
+//
+//  Created by Alexandre Madeira on 08/02/22.
+//
+
+import Foundation
+
+@MainActor
+class TVDetailsViewModel: ObservableObject {
+    private let service: NetworkService = NetworkService.shared
+    @Published private(set) var phase: DataFetchPhase<TVShow?> = .empty
+    var tvShow: TVShow? {
+        phase.value ?? nil
+    }
+    func load(id: Int) async {
+        if Task.isCancelled { return }
+        phase = .empty
+        do {
+            let tvShow = try await self.service.fetchTvShow(id: id)
+            phase = .success(tvShow)
+        } catch {
+            phase = .failure(error)
+        }
+    }
+}
