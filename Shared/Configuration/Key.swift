b6c0a88d53d3113a60c584ece9463af96a091679
Alexandre Madeira <contact@alexandremadeira.dev>
2022-03-24 15:08:43 -0300

    Add files via upload

---
+++ Shared/Configuration/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  Story
+//
+//  Created by Alexandre Madeira on 28/01/22.
+//  swiftlint:disable line_length
+
+import Foundation
+
+struct Key {
+    static let keyV3 = "" //TODO: use your own key
+    static let keyV4 = "" //TODO: use your own key
+}
