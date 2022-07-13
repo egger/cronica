b866d059bd071f921458636e140b3a4a74212f2b
Alexandre Madeira <contact@alexandremadeira.dev>
2022-07-13 11:10:46 -0300

    Update HeroImage.swift
    Moved HeroImage from AsyncImage to WebImage.

--- Shared/View/Components/HeroImage.swift
+++ Shared/View/Components/HeroImage.swift
@@ -6,47 +6,34 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct HeroImage: View {
     let url: URL?
     let title: String
     var blurImage: Bool = false
+    @State private var isPad: Bool = UIDevice.isIPad
     var body: some View {
-        AsyncImage(url: url,
-                   transaction: Transaction(animation: .easeInOut)) { phase in
-            if let image = phase.image {
-                ZStack {
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .transition(.opacity)
-                    if blurImage {
-                        Rectangle().fill(.ultraThickMaterial)
-                        Image(systemName: "eye.slash.fill")
-                            .foregroundColor(.white)
-                            .font(.system(size: 30))
-                    }
+        ZStack {
+            WebImage(url: url)
+                .resizable()
+                .placeholder {
+                    HeroImagePlaceholder(title: title)
                 }
-            } else if phase.error != nil {
-                ZStack {
-                    Rectangle().fill(.thickMaterial)
-                    ProgressView(title)
-                }
-            } else {
-                ZStack {
-                    Rectangle().fill(.thickMaterial)
-                    VStack {
-                        Text(title)
-                            .lineLimit(1)
-                            .padding(.bottom)
-                        Image(systemName: "film")
-                    }
-                    .padding()
-                    .foregroundColor(.secondary)
-                }
+                .aspectRatio(contentMode: .fill)
+                .transition(.opacity)
+            if blurImage {
+                Rectangle().fill(.ultraThinMaterial)
+                Image(systemName: "eye.slash.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
             }
         }
-                   
+        .frame(width: isPad ? DrawingConstants.padImageWidth : DrawingConstants.imageWidth,
+               height: isPad ? DrawingConstants.padImageHeight : DrawingConstants.imageHeight)
+        .cornerRadius(isPad ? DrawingConstants.padImageRadius : DrawingConstants.imageRadius)
+        .shadow(color: .black.opacity(DrawingConstants.shadowOpacity),
+                radius: DrawingConstants.shadowRadius)
     }
 }
 
@@ -55,4 +42,32 @@ struct HeroImage_Previews: PreviewProvider {
         HeroImage(url: ItemContent.previewContent.cardImageLarge,
                   title: ItemContent.previewContent.itemTitle)
     }
+}
+
+private struct HeroImagePlaceholder: View {
+    let title: String
+    var body: some View {
+        ZStack {
+            Rectangle().fill(.thickMaterial)
+            VStack {
+                Text(title)
+                    .lineLimit(1)
+                    .padding(.bottom)
+                Image(systemName: "film")
+            }
+            .padding()
+            .foregroundColor(.secondary)
+        }
+    }
+}
+
+private struct DrawingConstants {
+    static let shadowOpacity: Double = 0.2
+    static let shadowRadius: CGFloat = 5
+    static let imageWidth: CGFloat = 360
+    static let imageHeight: CGFloat = 210
+    static let imageRadius: CGFloat = 8
+    static let padImageWidth: CGFloat = 500
+    static let padImageHeight: CGFloat = 300
+    static let padImageRadius: CGFloat = 12
 }
