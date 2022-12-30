89e235083dc28a8791be0ed721242eeb836e858a
Alexandre Madeira <contact@alexandremadeira.dev>
2022-12-29 21:53:48 -0300

    Update WatchlistButtonView.swift
    Added alert confirmation to watchOS.

--- Shared/View/Components/Buttons/WatchlistButtonView.swift
+++ Shared/View/Components/Buttons/WatchlistButtonView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct WatchlistButtonView: View {
     @EnvironmentObject var viewModel: ItemContentViewModel
+    @State private var showConfirmationPopup = false
     var body: some View {
         Button(action: {
-            if let item = viewModel.content {
-                viewModel.updateWatchlist(with: item)
+#if os(watchOS)
+            if viewModel.isInWatchlist {
+                showConfirmationPopup = true
+            } else {
+                update()
             }
+#else
+            update()
+#endif
         }, label: {
             Label(viewModel.isInWatchlist ? "Remove from watchlist": "Add to watchlist",
                   systemImage: viewModel.isInWatchlist ? "minus.square" : "plus.square")
@@ -25,6 +32,17 @@ struct WatchlistButtonView: View {
         .tint(viewModel.isInWatchlist ? .red : .blue)
 #if os(iOS)
         .buttonBorderShape(.capsule)
+#elseif os(watchOS)
+        .alert("Are you sure?", isPresented: $showConfirmationPopup) {
+            Button("Confirm") { update() }
+            Button("Cancel") {  showConfirmationPopup = false }
+        }
 #endif
+    }
+    
+    private func update() {
+        if let item = viewModel.content {
+            viewModel.updateWatchlist(with: item)
+        }
     }
 }
