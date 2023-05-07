b9c602623ac6f21db79b6e6d8fefe600eb7325d8
Alexandre Madeira <contact@alexandremadeira.dev>
2023-05-07 19:54:58 -0300

    Update CronicaTests.swift

--- CronicaTests/CronicaTests.swift
+++ CronicaTests/CronicaTests.swift
@@ -23,97 +23,97 @@ final class CronicaTests: XCTestCase {
             persistence.save(item)
         }
         for item in ItemContent.examples {
-            XCTAssertTrue(persistence.isItemSaved(id: item.itemNotificationID))
+            XCTAssertTrue(persistence.isItemSaved(id: item.itemContentID))
         }
     }
     
     func testMarkAsWatched() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateWatched(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertTrue(persistence.isMarkedAsWatched(id: item.itemNotificationID))
+            XCTAssertTrue(persistence.isMarkedAsWatched(id: item.itemContentID))
         }
     }
     
     func testRemoveFromWatched() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateWatched(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertFalse(persistence.isMarkedAsWatched(id: item.itemNotificationID))
+            XCTAssertFalse(persistence.isMarkedAsWatched(id: item.itemContentID))
         }
     }
     
     func testMarkAsFavorite() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateFavorite(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertTrue(persistence.isMarkedAsFavorite(id: item.itemNotificationID))
+            XCTAssertTrue(persistence.isMarkedAsFavorite(id: item.itemContentID))
         }
     }
     
     func testRemoveFromFavorite() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateFavorite(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertFalse(persistence.isMarkedAsFavorite(id: item.itemNotificationID))
+            XCTAssertFalse(persistence.isMarkedAsFavorite(id: item.itemContentID))
         }
     }
     
     func testMarkAsArchive() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateArchive(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertTrue(persistence.isItemArchived(id: item.itemNotificationID))
+            XCTAssertTrue(persistence.isItemArchived(id: item.itemContentID))
         }
     }
     
     func testRemoveFromArchive() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updateArchive(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertFalse(persistence.isItemArchived(id: item.itemNotificationID))
+            XCTAssertFalse(persistence.isItemArchived(id: item.itemContentID))
         }
     }
     
     func testMarkAsPin() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updatePin(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertTrue(persistence.isItemPinned(id: item.itemNotificationID))
+            XCTAssertTrue(persistence.isItemPinned(id: item.itemContentID))
         }
     }
     
     func testRemoveFromPins() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.updatePin(for: item)
         }
         for item in ItemContent.examples {
-            XCTAssertFalse(persistence.isItemPinned(id: item.itemNotificationID))
+            XCTAssertFalse(persistence.isItemPinned(id: item.itemContentID))
         }
     }
     
     func testRemoveItemsFromWatchlist() {
         for item in ItemContent.examples {
-            guard let item = try? persistence.fetch(for: item.itemNotificationID) else { return }
+            guard let item = try? persistence.fetch(for: item.itemContentID) else { return }
             persistence.delete(item)
         }
         for item in ItemContent.examples {
-            XCTAssertFalse(persistence.isItemSaved(id: item.itemNotificationID))
+            XCTAssertFalse(persistence.isItemSaved(id: item.itemContentID))
         }
     }
     
