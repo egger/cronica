c45afb895db090cfa7867cc42dd7dc8adca6107d
Alexandre Madeira <contact@alexandremadeira.dev>
2023-07-12 21:13:37 -0300

    Reverted back Xcode Cloud sh

--- Shared/Configuration/Key.swift
+++ Shared/Configuration/Key.swift
@@ -11,23 +11,8 @@ import Foundation
 ///
 /// The values for each key is defined in an environment variable.
 struct Key {
-    #warning("You can also place your API Key here, the actual setup is just to make it work better with Xcode Cloud.")
-    static var tmdbApi: String {
-        let infoKey = "CRONICA_API_KEY_TMDB"
-        guard let apiKey = Bundle.main.infoDictionary?[infoKey] as? String else { return String() }
-        return apiKey
-    }
-    #warning("This is only for production code, you can remove it if you want to.")
-    static var telemetryClientKey: String? {
-        let infoKey = "CRONICA_API_KEY_TELEMETRYDECK"
-        guard let apiKey = Bundle.main.infoDictionary?[infoKey] as? String else { return nil }
-        if apiKey.isEmpty { return nil }
-        return apiKey
-    }
-    static var authorizationHeader: String? {
-        let infoKey = "CRONICA_API_KEY_TMDB_AuthorizationHeader"
-        guard let apiKey = Bundle.main.infoDictionary?[infoKey] as? String else { return nil }
-        if apiKey.isEmpty { return nil }
-        return apiKey
-    }
+    #warning("You can place your API Key here.")
+    static let tmdbApi: String = ""
+    static let telemetryClientKey: String? = ""
+    static let authorizationHeader: String?  = ""
 }
