49d386bcc1bc33824b0950e0dc5927fa80072c5f
Alexandre Madeira <contact@alexandremadeira.dev>
2024-04-13 20:07:18 -0300

    Removed unused values from Key.swift

--- Shared/Configuration/Key.swift
+++ Shared/Configuration/Key.swift
@@ -12,7 +12,5 @@ import Foundation
 /// The values for each key is defined in an environment variable.
 struct Key {
     static let tmdbApi = ""
-    static let telemetryClientKey: String? = ""
-    static let authorizationHeader: String? = ""
     static let aptabaseClientKey: String? = ""
 }
