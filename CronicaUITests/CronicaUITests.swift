cd2bacfa73423ec8abaa53a4bc9883ca79ca8a00
moataz-akram <moatazakram0@gmail.com>
2024-09-12 09:55:47 +0300

    add helper methods

--- CronicaUITests/CronicaUITests.swift
+++ CronicaUITests/CronicaUITests.swift
@@ -22,4 +22,31 @@ final class CronicaUITests: XCTestCase {
         super.tearDown()
     }
 
+    func NavigateToHomeTab() {
+        let tabBar = app.tabBars["Tab Bar"]
+        let homeButton = tabBar.buttons["Home"]
+        homeButton.tap()
+    }
+    
+    func navigateToTab(_ tab: Screens) {
+        let tabBar = app.tabBars["Tab Bar"]
+        
+        switch tab {
+        case .home:
+            tabBar.buttons["Home"].tap()
+        case .explore:
+            tabBar.buttons["Discover"].tap()
+        case .watchlist:
+            tabBar.buttons["Watchlist"].tap()
+        case .search:
+            tabBar.buttons["Search"].tap()
+        case .notifications:
+            NavigateToHomeTab()
+            app.buttons["Notifications"].tap()
+        case .settings:
+            NavigateToHomeTab()
+            app.buttons["Settings"].tap()
+        }
+    }
+
 }
